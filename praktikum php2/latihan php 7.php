0a5c3598598ffb169f5ef1ad33f0991a14dc1b8d
54m <84306375+samryn@users.noreply.github.com>
2021-09-08 12:51:12 +0700

    Update latihan php 7.php

--- praktikum php2/latihan php 7.php
+++ praktikum php2/latihan php 7.php
@@ -1,13 +1,13 @@
 <?php
-$arrBuah=array("Mangga", "Apel", "Pisang", "Jeruk");
+$arrBuah=array("Mangga ", "Apel ", "Pisang ", "Jeruk ");
 echo $arrBuah[0]; //Mangga
 echo $arrBuah[3]; //Jeruk
 
 $arrWarna=array();
-$arrWarna[]="Merah";
-$arrWarna[]="Biru";
-$arrWarna[]="Hijau";
-$arrWarna[]="Putih";
+$arrWarna[]=" Merah";
+$arrWarna[]= "Biru";
+$arrWarna[]=" Hijau";
+$arrWarna[]=" Putih";
 echo $arrWarna[0]; //Merah
 echo $arrWarna[2]; //Hijau
 ?>
