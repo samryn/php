3ae5d6de32cb02f90656c094383c4352f97c138a
54m <84306375+samryn@users.noreply.github.com>
2021-09-14 21:21:38 +0700

    Add files via upload

---
+++ modul php 3/latihan 2.php
@@ -0,0 +1,8 @@
+<?php
+$nilai=50;
+if($nilai >= 75){
+    echo "Nilai Anda $nilai, Anda LULUS";
+}else{
+    echo "Nilai Anda $nilai, Anda GAGAL";
+}
+?>
---
+++ modul php 3/latihan 3.php
@@ -0,0 +1,9 @@
+<?php
+$user="firdausa";
+$pass="123";
+if($user == "firdausa" && $pass == "123"){
+    echo "Login Berhasil";
+}else{
+    echo "Login Gagal";
+}
+?>
